6441315b41d358bd6968fc0dd41fb806439d04ed
tanamet.tpk <tanamet.tpk@gmail.com>
2019-07-20 17:11:58 +0700

    + install ampqlib

--- cart-management-service/server.js
+++ cart-management-service/server.js
@@ -3,6 +3,8 @@ var items = express();
 const server = items.server;
 const io = require('./socket')(server);
 
+require('./config/mqService')
+
 server.listen(80 , function() {
     console.log(`server run at http://localhost:80`);
 })
